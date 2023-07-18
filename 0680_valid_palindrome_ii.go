edac2bedc902d6d2ce18ba77031b4562e9f9b912
aldalee <hadoop_2020@163.com>
2023-07-18 18:46:05 +0800

    验证回文串II

---
+++ 0680_valid_palindrome_ii.go
@@ -0,0 +1,26 @@
+// https://leetcode.cn/problems/valid-palindrome-ii/
+// 验证回文串 II
+package main
+
+func validPalindrome(s string) bool {
+	l, r := 0, len(s)-1
+	for l < r {
+		if s[l] != s[r] {
+			return isPalindrome(s, l+1, r) || isPalindrome(s, l, r-1)
+		}
+		l++
+		r--
+	}
+	return true
+}
+
+func isPalindrome(s string, l, r int) bool {
+	for l < r {
+		if s[l] != s[r] {
+			return false
+		}
+		l++
+		r--
+	}
+	return true
+}
