0096420e2dc6fc84c31d5cd1874da6d2df1fc482
aldalee <hadoop_2020@163.com>
2023-08-04 01:24:12 +0800

    两个数组的交集

--- problem0349_intersection_of_two_arrays.go
+++ problem0349_intersection_of_two_arrays.go
@@ -2,26 +2,29 @@
 // 两个数组的交集
 package main
 
-func intersection(nums1 []int, nums2 []int) (ans []int) {
+func intersection(nums1 []int, nums2 []int) []int {
 	set := map[int]struct{}{}
 	res := map[int]struct{}{}
-	addToSet(nums1, set)
-	addToSet(nums2, res)
-	if len(set) > len(res) {
-		set, res = res, set
+	if len(nums1) > len(nums2) {
+		nums1, nums2 = nums2, nums1
 	}
-	for v := range set {
-		if _, ok := res[v]; ok {
-			ans = append(ans, v)
-		}
-	}
-	return
-}
-
-func addToSet(num []int, set map[int]struct{}) {
-	for _, v := range num {
+	for _, v := range nums1 {
 		set[v] = struct{}{}
 	}
+	for _, num := range nums2 {
+		if _, ok := set[num]; ok {
+			res[num] = struct{}{}
+		}
+	}
+	return convert(res)
+}
+
+func convert(set map[int]struct{}) []int {
+	ans := make([]int, 0, len(set))
+	for num := range set {
+		ans = append(ans, num)
+	}
+	return ans
 }
 
 // 面试题: 求两个有序数组的交集
