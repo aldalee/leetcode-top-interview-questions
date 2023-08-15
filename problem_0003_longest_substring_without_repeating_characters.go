835350897dc25ec8b57470b93455cd96c7233e0b
aldalee <hadoop_2020@163.com>
2023-08-15 23:15:00 +0800

    无重复字符的最长子串

---
+++ problem_0003_longest_substring_without_repeating_characters.go
@@ -0,0 +1,25 @@
+// https://leetcode.cn/problems/longest-substring-without-repeating-characters/
+// 无重复字符的最长子串
+package main
+
+func lengthOfLongestSubstring(s string) int {
+	var m [256]int
+	for i := 0; i < len(m); i++ {
+		m[i] = -1
+	}
+	res := 0
+	pre := -1
+	for idx := 0; idx < len(s); idx++ {
+		pre = max(pre, m[s[idx]])
+		res = max(res, idx-pre)
+		m[s[idx]] = idx
+	}
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
