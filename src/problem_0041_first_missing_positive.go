0c98229732c3cb520a0c63ec3f9bb6fae29b09c4
aldalee <hadoop_2020@163.com>
2023-08-24 10:44:52 +0800

    LeetCode高频题：缺失的第一个正数

---
+++ src/problem_0041_first_missing_positive.go
@@ -0,0 +1,23 @@
+// https://leetcode.cn/problems/first-missing-positive
+// 缺失的第一个正数
+package main
+
+func firstMissingPositive(nums []int) int {
+	// expect: nums[l] == l + 1
+	l, r := 0, len(nums)
+	for l < r {
+		if nums[l] == l+1 {
+			l++
+		} else if nums[l] <= l || nums[l] > r || nums[l] == nums[nums[l]-1] {
+			r--
+			nums[l] = nums[r]
+		} else {
+			swap(nums, l, nums[l]-1)
+		}
+	}
+	return l + 1
+}
+
+func swap(nums []int, i, j int) {
+	nums[i], nums[j] = nums[j], nums[i]
+}
