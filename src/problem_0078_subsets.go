15ccdc28266a34790ebfaad0436da7821c4570e1
aldalee <hadoop_2020@163.com>
2023-09-02 17:57:30 +0800

    LeetCode高频题：子集

--- src/problem_0078_subsets.go
+++ src/problem_0078_subsets.go
@@ -10,12 +10,9 @@ func subsets(nums []int) (res [][]int) {
 		if idx == len(nums) {
 			res = append(res, append([]int(nil), path...))
 		} else {
-			// 决定一: 不要当前的数，继续下一个分支
 			dfs(idx + 1)
-			// 决定二: 要当前的数，继续下一个分支
 			path = append(path, nums[idx])
 			dfs(idx + 1)
-			// 恢复现场
 			path = path[:len(path)-1]
 		}
 	}
