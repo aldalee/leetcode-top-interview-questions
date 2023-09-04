f4fd065520a0d6be13c6fc97f9713c709b57f17e
aldalee <hadoop_2020@163.com>
2023-09-04 18:07:30 +0800

    LeetCode高频题：二叉树的中序遍历（Morris遍历）

--- src/problem_0094_binary_tree_inorder_traversal.go
+++ src/problem_0094_binary_tree_inorder_traversal.go
@@ -2,16 +2,25 @@
 // 二叉树的中序遍历
 package main
 
+// based on morris traversal to inorder traversal
 func inorderTraversal(root *TreeNode) (res []int) {
-	var inorder func(root *TreeNode)
-	inorder = func(root *TreeNode) {
-		if root == nil {
-			return
+	cur, mostRight := root, root
+	for cur != nil {
+		mostRight = cur.Left
+		if mostRight != nil {
+			for mostRight.Right != nil && mostRight.Right != cur {
+				mostRight = mostRight.Right
+			}
+			if mostRight.Right == nil {
+				mostRight.Right = cur
+				cur = cur.Left
+				continue
+			} else {
+				mostRight.Right = nil
+			}
 		}
-		inorder(root.Left)
-		res = append(res, root.Val)
-		inorder(root.Right)
+		res = append(res, cur.Val)
+		cur = cur.Right
 	}
-	inorder(root)
 	return
 }
