9f5e3e8ab5d4149b18a4b9cef76b3c7007ab339a
aldalee <hadoop_2020@163.com>
2023-09-04 18:00:59 +0800

    LeetCode高频题：二叉树的中序遍历（常规解法）

---
+++ src/problem_0094_binary_tree_inorder_traversal.go
@@ -0,0 +1,17 @@
+// https://leetcode.cn/problems/binary-tree-inorder-traversal/
+// 二叉树的中序遍历
+package main
+
+func inorderTraversal(root *TreeNode) (res []int) {
+	var inorder func(root *TreeNode)
+	inorder = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		inorder(root.Left)
+		res = append(res, root.Val)
+		inorder(root.Right)
+	}
+	inorder(root)
+	return
+}
