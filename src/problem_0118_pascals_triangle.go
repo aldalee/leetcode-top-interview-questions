ebf183a5df38bf258996b1e56ec22accdcd44efb
aldalee <hadoop_2020@163.com>
2023-09-05 22:57:17 +0800

    LeetCode高频题：杨辉三角

---
+++ src/problem_0118_pascals_triangle.go
@@ -0,0 +1,16 @@
+// https://leetcode.cn/problems/pascals-triangle/
+// 杨辉三角
+package main
+
+// Generate Modify 'Generate' to 'generate' when submitting.
+func Generate(numRows int) [][]int {
+	res := make([][]int, numRows)
+	for i := range res {
+		res[i] = make([]int, i+1)
+		res[i][0], res[i][i] = 1, 1
+		for j := 1; j < i; j++ {
+			res[i][j] = res[i-1][j-1] + res[i-1][j]
+		}
+	}
+	return res
+}
