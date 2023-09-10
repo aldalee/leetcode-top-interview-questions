c33cf3d06c74a2eca896614bfa19a00ed07ec0f5
aldalee <hadoop_2020@163.com>
2023-09-10 13:47:16 +0800

    LeetCode高频题：加油站

--- src/problem_0134_gas_station.go
+++ src/problem_0134_gas_station.go
@@ -5,8 +5,7 @@ package main
 func canCompleteCircuit(gas, cost []int) (start int) {
 	n := len(gas)
 	for start < n {
-		sumOfGas := 0
-		cnt := 0
+		sumOfGas, cnt := 0, 0
 		for ; cnt < n; cnt++ {
 			cur := (start + cnt) % n
 			sumOfGas += gas[cur] - cost[cur]
