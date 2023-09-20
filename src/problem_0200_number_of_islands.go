6c5a034255cd83183491325b217af9a9ff62edd2
aldalee <hadoop_2020@163.com>
2023-09-21 00:44:53 +0800

    LeetCode高频题：岛屿数量（并查集）

--- src/problem_0200_number_of_islands.go
+++ src/problem_0200_number_of_islands.go
@@ -4,24 +4,45 @@ package main
 
 func numIslands(grid [][]byte) (islands int) {
 	row, col := len(grid), len(grid[0])
-	var connect func(i, j int)
-	connect = func(i, j int) {
-		if i < 0 || i == row || j < 0 || j == col || grid[i][j] == '0' {
-			return
-		}
-		grid[i][j] = '0'
-		connect(i-1, j)
-		connect(i+1, j)
-		connect(i, j-1)
-		connect(i, j+1)
+	set := make([]int, row*col)
+	for i := range set {
+		set[i] = i
 	}
 	for i := 0; i < row; i++ {
 		for j := 0; j < col; j++ {
 			if grid[i][j] == '1' {
+				idx := i*col + j
 				islands++
-				connect(i, j)
+				if j > 0 && grid[i][j-1] == '1' {
+					a, b := find(set, idx), find(set, idx-1)
+					if a != b {
+						union(set, a, b)
+						islands--
+					}
+				}
+				if i > 0 && grid[i-1][j] == '1' {
+					a, b := find(set, idx), find(set, idx-col)
+					if a != b {
+						union(set, a, b)
+						islands--
+					}
+				}
 			}
 		}
 	}
 	return
+}
+
+func find(set []int, v int) int {
+	if v != set[v] {
+		set[v] = find(set, set[v])
+	}
+	return set[v]
+}
+
+func union(set []int, a, b int) {
+	a, b = find(set, a), find(set, b)
+	if a != b {
+		set[a] = set[b]
+	}
 }
