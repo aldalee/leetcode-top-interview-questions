052f91fe2219c880a0718a95719ffc646233cb65
aldalee <hadoop_2020@163.com>
2023-09-20 16:21:17 +0800

    LeetCode高频题：位1的个数

--- src/problem_0191_number_of_1_bits.go
+++ src/problem_0191_number_of_1_bits.go
@@ -2,9 +2,18 @@
 // 位1的个数
 package main
 
-func hammingWeight(n uint32) (bits int) {
-	for bits = 0; n != 0; bits++ {
-		n &= n - 1
+func hammingWeight(n uint32) int {
+	masks := []uint32{0x55555555, 0x33333333, 0x0f0f0f0f, 0x00ff00ff, 0x0000ffff}
+	shifts := []uint32{1, 2, 4, 8, 16}
+	for i, mask := range masks {
+		n = n&mask + n>>shifts[i]&mask
 	}
-	return
+	return int(n)
 }
+
+//func hammingWeight(n uint32) (bits int) {
+//	for bits = 0; n != 0; bits++ {
+//		n &= n - 1
+//	}
+//	return
+//}
