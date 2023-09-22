0fb231f49c4bdc3b0d414b93d4a96af1d9e25636
aldalee <hadoop_2020@163.com>
2023-09-23 01:21:15 +0800

    LeetCode高频题：实现Trie树（前缀树）

---
+++ src/problem_0208_implement_trie_prefix_tree.go
@@ -0,0 +1,45 @@
+// https://leetcode.cn/problems/implement-trie-prefix-tree/
+// 实现Trie树（前缀树）
+package main
+
+type Trie struct {
+	nexts [26]*Trie
+	end   bool
+}
+
+func Constructor3() Trie {
+	return Trie{}
+}
+
+func (root *Trie) Insert(word string) {
+	node := root
+	for _, path := range word {
+		path -= 'a'
+		if node.nexts[path] == nil {
+			node.nexts[path] = &Trie{}
+		}
+		node = node.nexts[path]
+	}
+	node.end = true
+}
+
+func (root *Trie) Search(word string) bool {
+	node := root.searchPrefix(word)
+	return node != nil && node.end
+}
+
+func (root *Trie) StartsWith(prefix string) bool {
+	return root.searchPrefix(prefix) != nil
+}
+
+func (root *Trie) searchPrefix(prefix string) *Trie {
+	node := root
+	for _, path := range prefix {
+		path -= 'a'
+		if node.nexts[path] == nil {
+			return nil
+		}
+		node = node.nexts[path]
+	}
+	return node
+}
