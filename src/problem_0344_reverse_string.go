1ad187d0a62db018061fc53908b7b0c19b630038
aldalee <hadoop_2020@163.com>
2023-09-28 18:14:10 +0800

    LeetCode高频题：反转字符串

---
+++ src/problem_0344_reverse_string.go
@@ -0,0 +1,12 @@
+// https://leetcode.cn/problems/reverse-string
+// 反转字符串
+package main
+
+func reverseString(s []byte) {
+	l, r := 0, len(s)-1
+	for l < r {
+		s[l], s[r] = s[r], s[l]
+		l++
+		r--
+	}
+}
