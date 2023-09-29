1ba23c8d9f03380953b6ce735ec92a952d2bae42
aldalee <hadoop_2020@163.com>
2023-09-29 14:15:54 +0800

    LeetCode高频题：Fizz Buzz

---
+++ src/problem_0412_fizz_buzz.go
@@ -0,0 +1,20 @@
+// https://leetcode.cn/problems/fizz-buzz/
+// Fizz Buzz
+package main
+
+import "strconv"
+
+func fizzBuzz(n int) (res []string) {
+	for i := 1; i <= n; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			res = append(res, "FizzBuzz")
+		} else if i%3 == 0 {
+			res = append(res, "Fizz")
+		} else if i%5 == 0 {
+			res = append(res, "Buzz")
+		} else {
+			res = append(res, strconv.Itoa(i))
+		}
+	}
+	return
+}
