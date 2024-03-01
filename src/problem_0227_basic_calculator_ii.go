a1e2d7549209f1e9bff1af1a983f6affb5686464
aldalee <hadoop_2020@163.com>
2024-03-01 11:56:28 +0800

    LeetCode高频题：基本计算器 II

---
+++ src/problem_0227_basic_calculator_ii.go
@@ -0,0 +1,30 @@
+package main
+
+import "unicode"
+
+func calculate(s string) (ans int) {
+	var stack []int
+	preSign, num := '+', 0
+	for i, ch := range s {
+		if unicode.IsDigit(ch) {
+			num = num*10 + int(ch-'0')
+		}
+		if !unicode.IsDigit(ch) && ch != ' ' || i == len(s)-1 {
+			switch preSign {
+			case '+':
+				stack = append(stack, num)
+			case '-':
+				stack = append(stack, -num)
+			case '*':
+				stack[len(stack)-1] *= num
+			default:
+				stack[len(stack)-1] /= num
+			}
+			preSign, num = ch, 0
+		}
+	}
+	for _, v := range stack {
+		ans += v
+	}
+	return
+}
