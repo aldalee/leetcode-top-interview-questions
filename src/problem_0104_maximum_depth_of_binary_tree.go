70d8ff63cf953e81b3b1cc39381c8dd372a6217f
aldalee <hadoop_2020@163.com>
2024-03-04 10:30:08 +0800

    LeetCode高频题：二叉树的最大深度

--- src/problem_0104_maximum_depth_of_binary_tree.go
+++ src/problem_0104_maximum_depth_of_binary_tree.go
@@ -6,5 +6,29 @@ func maxDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
-	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+	maxDepth, curDepth := 0, 0
+	for cur := root; cur != nil; {
+		if cur.Left == nil {
+			curDepth++
+			cur = cur.Right
+		} else {
+			rightmost := cur.Left
+			count := 1
+			for rightmost.Right != nil && rightmost.Right != cur {
+				count++
+				rightmost = rightmost.Right
+			}
+			if rightmost.Right == nil {
+				curDepth++
+				rightmost.Right = cur
+				cur = cur.Left
+			} else {
+				maxDepth = max(maxDepth, curDepth)
+				curDepth -= count
+				rightmost.Right = nil
+				cur = cur.Right
+			}
+		}
+	}
+	return max(maxDepth, curDepth)
 }
