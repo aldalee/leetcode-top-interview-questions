0388e306521facb5beb70f3048ef1e6243cf7ccc
aldalee <hadoop_2020@163.com>
2024-03-04 10:36:35 +0800

    LeetCode高频题：二叉搜索树中第K小的元素

--- src/problem_0230_kth_smallest_element_in_bst.go
+++ src/problem_0230_kth_smallest_element_in_bst.go
@@ -3,8 +3,7 @@
 package main
 
 func kthSmallest(root *TreeNode, k int) int {
-	cur := root
-	for cur != nil {
+	for cur := root; cur != nil; {
 		rightmost := cur.Left
 		if rightmost != nil {
 			for rightmost.Right != nil && rightmost.Right != cur {
