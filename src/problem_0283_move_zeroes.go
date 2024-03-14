b0a5b3f662b50629bee9d20d1529a4bdbb2bfae9
aldalee <hadoop_2020@163.com>
2024-03-14 10:17:41 +0800

    LeetCode高频题：移动零

--- src/problem_0283_move_zeroes.go
+++ src/problem_0283_move_zeroes.go
@@ -3,7 +3,7 @@
 package main
 
 func moveZeroes(nums []int) {
-	for l, r, n := 0, 0, len(nums); r < n; r++ {
+	for l, r := 0, 0; r < len(nums); r++ {
 		if nums[r] != 0 {
 			nums[l], nums[r] = nums[r], nums[l]
 			l++
