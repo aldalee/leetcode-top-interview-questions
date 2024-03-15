19beb64fd6c4a10faca3457cffb8ffe7aa6bac1f
aldalee <hadoop_2020@163.com>
2024-03-15 16:49:14 +0800

    LeetCode高频题：最长递增子序列

--- src/problem_0300_longest_increasing_subsequence.go
+++ src/problem_0300_longest_increasing_subsequence.go
@@ -2,25 +2,16 @@
 // 最长递增子序列
 package main
 
+import "sort"
+
 func lengthOfLIS(nums []int) int {
 	var ends []int
-	// find the first position that greater than or equal num in ends
-	var binarySearchLeftIdx = func(num int) int {
-		idx := -1
-		for l, r := 0, len(ends)-1; l <= r; {
-			m := l + ((r - l) >> 1)
-			if ends[m] >= num {
-				idx = m
-				r = m - 1
-			} else {
-				l = m + 1
-			}
-		}
-		return idx
-	}
 	for i := 0; i < len(nums); i++ {
-		idx := binarySearchLeftIdx(nums[i])
-		if idx == -1 {
+		// find the first position that greater than or equal num in ends
+		idx := sort.Search(len(ends), func(j int) bool {
+			return ends[j] >= nums[i]
+		})
+		if idx == len(ends) {
 			ends = append(ends, nums[i])
 		} else {
 			ends[idx] = nums[i]
